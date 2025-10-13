98880b8a2eebcf6effee1c805bc6ea6c7eb9322f
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2025-10-13 15:27:57 +0200

    Add center on location button

--- lib/widgets/gems_map.dart
+++ lib/widgets/gems_map.dart
@@ -12,23 +12,75 @@ class GemsMap extends StatefulWidget {
   final Function(LatLng position)? onTapCallback;
   final bool hasLocationPermission;
 
-  const GemsMap(
-      {super.key, this.markers, this.onTapCallback, required this.hasLocationPermission});
+  const GemsMap({
+    super.key,
+    this.markers,
+    this.onTapCallback,
+    required this.hasLocationPermission,
+  });
 
   @override
   State<StatefulWidget> createState() => _MapWidgetState();
 }
 
-class _MapWidgetState extends State<GemsMap> with SingleTickerProviderStateMixin {
+class _MapWidgetState extends State<GemsMap>
+    with SingleTickerProviderStateMixin {
   final MapController _mapController = MapController();
-  LatLng _mapCenter = defaultLocation;
-  LatLng? _preciseLocation;
   final double _currentZoom = 16;
+  late AnimationController _mapMoveController;
+  Tween<double>? _latTween;
+  Tween<double>? _lngTween;
+  Tween<double>? _zoomTween;
+  CurvedAnimation? _mapAnimation;
+  LatLng _mapCenter = defaultLocation;
+  bool _isOnCurrentLocation = false;
 
   @override
   void initState() {
     super.initState();
     _fetchLocation();
+
+    _mapMoveController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _mapMoveController.addListener(_mapMoveControllerListener);
+
+    _mapAnimation = CurvedAnimation(
+      parent: _mapMoveController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void _mapMoveControllerListener() {
+    if (_latTween == null ||
+        _lngTween == null ||
+        _zoomTween == null ||
+        _mapAnimation == null) {
+      return;
+    }
+
+    _mapController.move(
+      LatLng(
+        _latTween!.evaluate(_mapAnimation!),
+        _lngTween!.evaluate(_mapAnimation!),
+      ),
+      _zoomTween!.evaluate(_mapAnimation!),
+    );
+
+    final finalPos = LatLng(_latTween!.end!, _lngTween!.end!);
+    if (_mapController.camera.center == finalPos) {
+      setState(() {
+        _isOnCurrentLocation = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _mapMoveController.dispose();
+    super.dispose();
   }
 
   Future<void> _fetchLocation() async {
@@ -38,35 +90,19 @@ class _MapWidgetState extends State<GemsMap> with SingleTickerProviderStateMixin
 
     try {
       final lastPosition = await GeolocatorService.getLastLocation();
-      if (lastPosition != null)
-      {
+      if (lastPosition != null) {
         if (!mounted) return;
 
         setState(() {
-          _mapCenter = LatLng(
-            lastPosition.latitude,
-            lastPosition.longitude,
-          );
-
-          _preciseLocation = LatLng(
-            lastPosition.latitude,
-            lastPosition.longitude,
-          );
+          _mapCenter = LatLng(lastPosition.latitude, lastPosition.longitude);
         });
 
         _mapController.move(_mapCenter, _currentZoom);
+
+        setState(() {
+          _isOnCurrentLocation = true;
+        });
       }
-
-      final highAccuracyPosition = await GeolocatorService.getCurrentLocation(accuracy: LocationAccuracy.high);
-
-      if (!mounted) return;
-      setState(() {
-        _preciseLocation = LatLng(
-          highAccuracyPosition.latitude,
-          highAccuracyPosition.longitude,
-        );
-      });
-
     } catch (e) {
       debugPrint("Error fetching location: $e");
     }
@@ -74,6 +110,21 @@ class _MapWidgetState extends State<GemsMap> with SingleTickerProviderStateMixin
 
   // https://stackoverflow.com/a/73210147/16052290
   // modified to work by me :D
+  void _animatedMapMove(LatLng destLocation, double destZoom) {
+    // Create some tweens. These serve to split up the transition from one location to another.
+    // In our case, we want to split the transition be<tween> our current map center and the destination.
+    _latTween = Tween<double>(
+      begin: _mapCenter.latitude,
+      end: destLocation.latitude,
+    );
+    _lngTween = Tween<double>(
+      begin: _mapCenter.longitude,
+      end: destLocation.longitude,
+    );
+    _zoomTween = Tween<double>(begin: _currentZoom, end: destZoom);
+
+    _mapMoveController.forward(from: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -83,35 +134,72 @@ class _MapWidgetState extends State<GemsMap> with SingleTickerProviderStateMixin
         initialCenter: _mapCenter,
         initialZoom: _currentZoom,
         onPositionChanged: (mapCamera, hasGesture) {
-          if (widget.onTapCallback != null) widget.onTapCallback!(mapCamera.center);
-        },
+          _mapCenter = mapCamera.center;
 
+          if (widget.onTapCallback != null) {
+            widget.onTapCallback!(mapCamera.center);
+          }
+
+          setState(() {
+            _isOnCurrentLocation = false;
+          });
+        },
       ),
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: userAgent,
         ),
-          MarkerLayer(markers: [
-            ...?widget.markers,
-          ]),
+        MarkerLayer(markers: [...?widget.markers]),
         RichAttributionWidget(
           attributions: [
             TextSourceAttribution(
               'OpenStreetMap contributors',
               onTap: () async {
-                if (!await launchUrl(Uri.parse('https://openstreetmap.org/copyright'))) {
-                  throw Exception("Could not launch https://openstreetmap.org/copyright");
+                if (!await launchUrl(
+                  Uri.parse('https://openstreetmap.org/copyright'),
+                )) {
+                  throw Exception(
+                    "Could not launch https://openstreetmap.org/copyright",
+                  );
                 }
               },
               prependCopyright: true,
-            )
+            ),
           ],
           showFlutterMapAttribution: false,
         ),
         CurrentLocationLayer(
           alignPositionOnUpdate: AlignOnUpdate.never,
           alignDirectionOnUpdate: AlignOnUpdate.never,
+        ),
+        Positioned(
+          left: 16,
+          bottom: 16,
+          child: CircleAvatar(
+            radius: 32,
+            backgroundColor: Theme.of(context).primaryColor,
+            child: IconButton(
+              onPressed: () async {
+                final lastPosition = await GeolocatorService.getLastLocation();
+
+                if (lastPosition == null) return;
+
+                final position = LatLng(
+                  lastPosition.latitude,
+                  lastPosition.longitude,
+                );
+                _animatedMapMove(position, 16);
+              },
+              icon: Icon(
+                _isOnCurrentLocation
+                    ? Icons.my_location
+                    : Icons.location_searching,
+                color: Theme.of(context).canvasColor,
+                size: 24,
+              ),
+            ),
+          ),
         ),
       ],
     );
