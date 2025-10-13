3a614eb751bb85252d847da343be41b081286db7
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2025-10-13 15:54:52 +0200

    Add cluster locations support

--- lib/widgets/gems_map.dart
+++ lib/widgets/gems_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:hidden_gem/constants.dart';
 import 'package:hidden_gem/services/geo_locator_service.dart';
@@ -150,7 +151,34 @@ class _MapWidgetState extends State<GemsMap>
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: userAgent,
         ),
-        MarkerLayer(markers: [...?widget.markers]),
+        CurrentLocationLayer(
+          alignPositionOnUpdate: AlignOnUpdate.never,
+          alignDirectionOnUpdate: AlignOnUpdate.never,
+        ),
+        MarkerClusterLayerWidget(
+          options: MarkerClusterLayerOptions(
+            maxClusterRadius: 45,
+            size: const Size(40, 40),
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(50),
+            maxZoom: 15,
+            markers: [...?widget.markers],
+            builder: (context, markers) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: Center(
+                  child: Text(
+                    markers.length.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
         RichAttributionWidget(
           attributions: [
             TextSourceAttribution(
@@ -168,10 +196,6 @@ class _MapWidgetState extends State<GemsMap>
             ),
           ],
           showFlutterMapAttribution: false,
-        ),
-        CurrentLocationLayer(
-          alignPositionOnUpdate: AlignOnUpdate.never,
-          alignDirectionOnUpdate: AlignOnUpdate.never,
         ),
         Positioned(
           left: 16,
